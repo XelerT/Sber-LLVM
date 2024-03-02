11510d32babad5097ee5d121eb7eb24bb501dd12
Alexander.Taranov <taranov.ad@phystech.edu>
2024-03-02 17:17:34 +0300

    Edited error msgs.

--- include/frontend/error_hadl.hpp
+++ include/frontend/error_hadl.hpp
@@ -3,43 +3,56 @@
 #include <string>
 #include <cstring>
 
-#define RED_C    "\033[91m"
-#define GREEN_C  "\033[32m"
-#define BLUE_C   "\033[36m"
-#define YELLOW_C "\033[33m"
-#define RESET_C  "\033[0m"
+#include "utils/utils.hpp"
 
 //-----------------------------------------------------------------------------------------
 
-namespace frontend {
+namespace frontend 
+{
 
-struct error_t {
-    std::string msg_;
-    const std::string prog_str_ = "";
-    std::string lex_type_ = "";
-    yy::parser::location_type loc_;
+struct error_t 
+{
+        std::string msg_ {};
+        const std::string prog_str_ {};
+        std::string lex_type_ {};
+        yy::parser::location_type loc_;
+        
+        const CONSOLE_CLRS colors {};
 
-    error_t(std::string msg, const std::string prog_str, std::string lex_type, yy::parser::location_type loc) :
-                        msg_(msg), prog_str_(prog_str), loc_(loc), lex_type_(lex_type) {}
+        char arrow_len = 5;
 
-    error_t(std::string msg, std::string lex_type, yy::parser::location_type loc) :
-                        msg_(msg), loc_(loc), lex_type_(lex_type) {}
+        error_t (std::string msg, const std::string prog_str, 
+                 std::string lex_type, yy::parser::location_type loc):
+                msg_(msg), prog_str_(prog_str), loc_(loc), lex_type_(lex_type) {}
 
-    void pretty_error_msg() const {
+        error_t (std::string msg, std::string lex_type, yy::parser::location_type loc):
+                msg_(msg), loc_(loc), lex_type_(lex_type) {}
 
-        std::cerr << "---------------------------------------------------------------\n";
-        std::cerr << "| " << RED_C << msg_ << RESET_C <<'|' << '\n';
-        std::cerr << "| in line: " << loc_.end.line << '\n';
-        std::cerr << "| lexem_type : "<< lex_type_ << '\n';
-
-        std::cerr << "| " << prog_str_  << '\n';
-        for (int i = 0; i < std::strlen("| ") + loc_.begin.column; i++) {
-            std::cerr << " ";
+        void print_h_boarder () const 
+        {
+                for (auto i = msg_.length(); i > 0; i--)
+                        std::cerr << "-";
+                std::cerr << "\n";
         }
-        std::cerr << RED_C << "^" << RESET_C << '\n';
-        std::cerr << "---------------------------------------------------------------\n";
 
-    }
+        void pretty_error_msg () const 
+        {
+                print_h_boarder();
+
+                std::cerr << colors.RED << msg_ << colors.DEFAULT << std::endl;
+                std::cerr << "In line: " << loc_.end.line << '\n';
+                std::cerr << "Lexem type : " << lex_type_ << '\n';
+
+                for (int i = 0; i < sizeof(" ") + + loc_.begin.column - arrow_len; i++)
+                        std::cerr << " ";
+
+                for (int i = 0; i < arrow_len; i++)
+                        std::cerr << colors.RED << "-" << colors.DEFAULT;
+
+                std::cerr << colors.RED << "^" << colors.DEFAULT << std::endl;
+
+                print_h_boarder();
+        }
 };
 
 }
